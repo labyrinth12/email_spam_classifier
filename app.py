e672dcd6ea0eec860d126fa8f607b0d38577fafa
Nivedita Tiwari <162312394+labyrinth12@users.noreply.github.com>
2024-04-22 14:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk download('punkt')
+nltk.download('punkt')
 ps=PorterStemmer()
 def transform_text(text):
   text=text.lower()
